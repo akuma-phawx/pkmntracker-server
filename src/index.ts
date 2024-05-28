4a91d2ac195178fc131fe16bf6dfadfb47b81717
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2024-05-28 20:03:07 +0200

    feat/user cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express, { Express } from "express";
 import { config } from "dotenv";
 import cardRoutes from "./routes/cards";
 import setRoutes from "./routes/sets";
+import cors from "cors";
+
 config();
 const app: Express = express();
 // Middleware to parse JSON bodies into `req.body`
 app.use(express.json());
+app.use(cors());
 
 // Mount API routes
 app.use("/api/cards", cardRoutes);
