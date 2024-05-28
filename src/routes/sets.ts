435090715a2f17c83a4afac01e15fae562982a2f
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2024-05-28 23:33:33 +0200

    feat: search set by name

--- src/routes/sets.ts
+++ src/routes/sets.ts
@@ -32,4 +32,21 @@ router.get("/:id", async (req: Request, res: Response) => {
   }
 });
 
+//find set by name
+router.get("/search/sets/:name", async (req: Request, res: Response) => {
+  const setName = req.params.name;
+  try {
+    const { rows } = await pool.query("SELECT * FROM sets WHERE name = $1", [
+      setName,
+    ]);
+    if (rows.length === 0) {
+      res.status(404).json({ message: "Set not found" });
+    } else {
+      res.json(rows[0]);
+    }
+  } catch (error) {
+    console.error("Error fetching set:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 export default router;
